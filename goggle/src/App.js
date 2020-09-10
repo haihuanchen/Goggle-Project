1c60d5b22de3fa2c8845d64d1f0e4e7d3ae52898
haihuanchen <haihuan.chen@hotmail.com>
2020-09-10 13:20:35 -0400

    added routers

--- goggle/src/App.js
+++ goggle/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to Goggle Apps</h1>
-
-      {/* Home page with the search bar*/}
-      <Home />
-      {/* Search Page with search results */}
+      <Router>
+        <Switch>
+          <Route path='/search'>
+            <h3>This is the search page</h3>
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- goggle/src/pages/Home.js
+++ goggle/src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div className='home'>
             <h3>This is the Home Page</h3>
+
+            <div className='home-header'>
+                <div className='home-headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className='home-headerRight'>
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+
+                </div>
+            </div>
+
+            <div className='home-body'>
+
+            </div>
         </div>
     )
 }
