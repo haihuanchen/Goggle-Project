a169772e32a4aab96723ec97439d90763344170c
haihuanchen <haihuan.chen@hotmail.com>
2020-09-10 13:05:48 -0400

    added a home page

--- goggle/src/App.js
+++ goggle/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome to Goggle Apps</h1>
+
+      {/* Home page with the search bar*/}
+      <Home />
+      {/* Search Page with search results */}
     </div>
   );
 }
---
+++ goggle/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css'
+
+function Home() {
+    return (
+        <div className='home'>
+            <h3>This is the Home Page</h3>
+        </div>
+    )
+}
+
+export default Home;
