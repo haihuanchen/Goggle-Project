5842fbd5d2c8fd2b425ff2791fd9eb13336e5d68
haihuanchen <haihuan.chen@hotmail.com>
2020-09-17 16:43:29 -0400

    search page stylings

--- goggle/src/pages/SearchPage.js
+++ goggle/src/pages/SearchPage.js
@@ -2,17 +2,80 @@ import React from 'react';
 import './SearchPage.css'
 import { useStateValue } from '../StateProvider';
 import useGoogleSearch from '../useGoogleSearch';
+import Response from "../response";
+import {Link} from "react-router-dom";
+import Search from "../components/Search";
+import SearchIcon from '@material-ui/icons/Search';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import RoomIcon from '@material-ui/icons/Room';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import ImageIcon from '@material-ui/icons/Image';
+import DescriptionIcon from '@material-ui/icons/Description';
 
 function SearchPage() {
     const [{term}, dispatch] = useStateValue();
-    const { data } = useGoogleSearch(term);
+
+    //Live API call
+    // const { data } = useGoogleSearch(term);
+
+    //use previous search results for development purpose since API only accepts 100 calls per day
+    const data = Response;
+    
+    console.log(data)
     
     return (
         <div className='searchPage'>
             <div className='searchPage-header'>
-                <h1>{term}</h1>
-            </div>
+                <Link to="/">
+                    <img
+                        className='searchPage-logo' 
+                        src="https://computerhistory.org/wp-content/uploads/2019/06/New-Google-Logo-great.jpg"
+                        alt='goggle logo'
+                    />
+                </Link>
+                <div className='searchPage-headerBody'>
+                    <Search hideButtons />
 
+                    <div className='searchPage-options'>
+                        <div className='searchPage-optionsLeft'>
+                            <div className='searchPage-option'>
+                                <SearchIcon />
+                                <Link to='/all'>All</Link>
+                            </div>
+                            <div className='searchPage-option'>
+                                <DescriptionIcon />
+                                <Link to='/news'>News</Link>
+                            </div>
+                            <div className='searchPage-option'>
+                                <ImageIcon />
+                                <Link to='/images'>Images</Link>
+                            </div>
+                            <div className='searchPage-option'>
+                                <LocalOfferIcon />
+                                <Link to='/shopping'>Shopping</Link>
+                            </div>
+                            <div className='searchPage-option'>
+                                <RoomIcon />
+                                <Link to='/maps'>Maps</Link>
+                            </div>
+                            <div className='searchPage-option'>
+                                <MoreVertIcon />
+                                <Link to='/more'>More</Link>
+                            </div>
+                        </div>
+
+                        <div className='searchPage-optionsRight'>
+                            <div className='searchPage-option'>
+                                <Link to='/settings'>Settings</Link>
+                            </div>
+                            <div className='searchPage-option'>
+                                <Link to='/tools'>Tools</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
             <div className='searchPage-results'>
 
             </div>
