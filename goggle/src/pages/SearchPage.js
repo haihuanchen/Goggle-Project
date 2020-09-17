a88b88d3895c8921057ac0d7d17c3deec12fd11b
haihuanchen <haihuan.chen@hotmail.com>
2020-09-17 13:43:09 -0400

    added google context api

--- goggle/src/pages/SearchPage.js
+++ goggle/src/pages/SearchPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './SearchPage.css'
 import { useStateValue } from '../StateProvider';
+import useGoogleSearch from '../useGoogleSearch';
 
 function SearchPage() {
     const [{term}, dispatch] = useStateValue();
-
+    const { data } = useGoogleSearch(term);
+    
     return (
         <div className='searchPage'>
             <div className='searchPage-header'>
